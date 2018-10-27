4b789caa4d68e48bd382447fa58299fae4f693e8
Arsalan Fakhar Siddiqui <42144377+arsalanfakhar@users.noreply.github.com>
2018-10-27 05:25:10 +0500

    Add files via upload

---
+++ 2nd website/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+	$(window).load(function(){
+		
+		$('.flexslider').flexslider({
+			animation: "slide",
+			directionNav: false
+			
+		});
+		
+	});
+			
+});
+
+smoothScroll.init({
+	speed:1300,
+	easing:'easeOutQuad'
+});
