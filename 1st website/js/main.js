0bdc6a6fb56dbe840187c2898bea8c80e92a9742
Arsalan Fakhar Siddiqui <42144377+arsalanfakhar@users.noreply.github.com>
2018-10-27 05:16:14 +0500

    Add files via upload

---
+++ 1st website/js/main.js
@@ -0,0 +1,131 @@
+$(document).ready(function() {
+	$('section#screenshot a').on('click',function () {
+		$('div#modal img').attr('src', $(this).attr('data-image-url') );
+
+	});	
+	
+	var nav=$('.navbar-fixed-top');
+	var distance=$('.navbar-fixed-top').offset();
+
+		if(distance.top>=300)
+		{
+			nav.addClass('effect');
+		}
+	/*console.log(distance);*/
+	$(window).scroll(function() {
+		var scroll=$(window).scrollTop();
+		if(scroll>=400){
+		   nav.addClass('effect');
+		   }
+		else{
+		   nav.removeClass('effect');
+		   }
+		
+	});
+	$('#about .blue-circle').waypoint(function() {
+		
+		$('#about .blue-circle').addClass('animated fadeInUp');
+	}, {
+		
+		offset:'50%'
+	});
+	$('#features .blue-circle').waypoint(function() {
+		
+		$(this.element).addClass('animated fadeInUp');
+	}, {
+		
+		offset:'50%'
+	});
+	$('.features-image img').waypoint(function() {
+		
+		$('.features-image img').addClass('animated rubberBand');
+	}, {
+		
+		offset:'50%'
+	}); 
+	$('#screenshot .col-sm-4').waypoint(function() {
+		
+		$(this.element).addClass('animated zoomIn');
+		$(this.element).css({'opacity':1});
+	}, {
+		
+		offset:'50%'
+	}); 
+	$('#download div.phone img').waypoint(function() {
+		
+		$(this.element).addClass('animated fadeInRight');
+	}, {
+		
+		offset:'50%'
+	});
+	$('#download .platforms div').waypoint(function() {
+		
+		$(this.element).addClass('animated fadeInUp');
+	}, {
+		
+		offset:'50%'
+	});
+	$('#support>p').waypoint(function() {
+		
+		$(this.element).addClass('animated fadeInUp');
+	}, {
+		
+		offset:'50%'
+	});
+	$('#support .col-md-8').waypoint(function() {
+		
+		$(this.element).addClass('animated fadeInRight');
+	}, {
+		
+		offset:'50%'
+	});
+	$('#form').bootstrapValidator({
+		message:'This value is not valid',
+		feedbackIcons:{
+			valid:'glyphicon glyphicon-ok',
+			invalid:'glyphicon glyphicon-remove',
+			validating:'glyphicon glyphicon-refresh'
+		},
+		fields:{
+			name:{
+				validators:{
+					notEmpty:{
+						message:'This field is required and cannot be empty'
+					}
+				}
+			},
+			email:{
+				validators:{
+					notEmpty:{
+						message:'This field is required and cannot be empty'
+					},
+					emailAddress:{
+						message:'The input is not a valid address'
+					}
+				}
+			},
+			message:{
+				validators:{
+					notEmpty:{
+						message:'The message cannot be empty,who sends an empty message???'
+					}
+				}
+			}
+		}
+	}).on('sucess.form.bv',function(e) {
+		 	e.preventDefault();
+		var $form=$(e.target);
+		var bv=$form.data('bootstrapValidator');
+		$.post($form.attr('action'), $form.serialize(), function(result) {
+			console.log(result);
+		},'json' );
+	});
+});
+
+smoothScroll.init({
+    speed:700,
+    easing:'easeInOutQuad',
+	updateURL:false,
+	offset:0
+});
+
